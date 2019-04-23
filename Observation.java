a6b32b522359778650d2f8debbb97fe03461611e
tada195908 <48470775+tada195908@users.noreply.github.com>
2019-04-23 18:30:35 +0900

    Add files via upload

---
+++ Observation.java
@@ -0,0 +1,56 @@
+package kuramoto;
+
+import java.awt.geom.Point2D;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.util.List;
+import myLib.utils.FileIO;
+import myLib.utils.Utils;
+
+/**
+ *
+ * @author tadaki
+ */
+public class Observation {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.io.IOException
+     */
+    public static void main(String[] args) throws IOException {
+        //振動子系の定義
+        int n = 32;
+        int tmax = 10000;
+        double h = 0.001;
+        double theta[] = new double[n];
+        double omega[] = new double[n];
+        double k = 5.;
+        for (int i = 0; i < n; i++) {
+            omega[i] = 5. * (0.8 + 0.4 * Math.random());
+            theta[i] = 2. * Math.PI * Math.random();
+        }
+        Kuramoto sys = new Kuramoto(theta, omega, k);
+        
+        //実行
+        List<Point2D.Double> plist = Utils.createList();
+        for (int t = 0; t < tmax; t++) {
+            double th[] = sys.update(h);
+            double d = 0.;
+            for (int i = 0; i < n-1; i++) {
+                for (int j = i+1; j < n; j++) {
+                    d += Math.pow(Math.sin(th[i] - th[j]), 2.);
+                }
+            }
+            plist.add(new Point2D.Double(t, d));
+        }
+        //ファイへ出力
+        String filename = Observation.class.getSimpleName()
+                + "-" + String.valueOf(k) + "-output.txt";
+        try (BufferedWriter out = FileIO.openWriter(filename)) {
+            for (Point2D.Double p : plist) {
+                FileIO.writeSSV(out, p.x, p.y);
+            }
+        }
+    }
+
+}
