53894681db644f7dd953f226c6dc415615783526
tada195908 <48470775+tada195908@users.noreply.github.com>
2023-03-15 17:36:09 +0900

    Add files via upload

--- src/interfaceExample/UseAnonymousClass.java
+++ src/interfaceExample/UseAnonymousClass.java
@@ -12,8 +12,9 @@ public class UseAnonymousClass {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //匿名クラスの利用
+        //Using anonymous class
         DoubleFunction<Double> function = new DoubleFunction<Double>(){
+            @Override
             public Double apply(double v){
                 return v*v;
             }
--- src/interfaceExample/UseLambda.java
+++ src/interfaceExample/UseLambda.java
@@ -12,7 +12,6 @@ public class UseLambda {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        //ラムダ式の利用
         DoubleFunction<Double> function = x -> x * x;
         System.out.println(function.apply(2.));
     }
